af82883a0882888c025057704ff8af48fce74c98
RedMuri <kartoev.04@bk.ru>
2022-10-24 20:56:20 +0300

    added fragments for AuthActivity

--- app/src/main/java/com/example/homehelper/presentation/screens/AuthActivity.kt
+++ app/src/main/java/com/example/homehelper/presentation/screens/AuthActivity.kt
@@ -1,11 +1,12 @@
-package com.example.homehelper
+package com.example.homehelper.presentation.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.homehelper.R
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
     }
 }

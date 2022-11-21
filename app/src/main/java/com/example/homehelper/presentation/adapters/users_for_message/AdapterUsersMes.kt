0c3479fa9358334ac0354cb43a4c7e08f3af3108
RedMuri <kartoev.04@bk.ru>
2022-11-21 11:30:14 +0300

    add: created adapter for users

---
+++ app/src/main/java/com/example/homehelper/presentation/adapters/users_for_message/AdapterUsersMes.kt
@@ -0,0 +1,30 @@
+package com.example.homehelper.presentation.adapters.chats
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.example.homehelper.R
+import com.example.homehelper.databinding.ItemUserMesBinding
+import com.example.homehelper.domain.entities.User
+import com.example.homehelper.presentation.adapters.users_for_message.UserMesItemDiffCallback
+import com.example.homehelper.presentation.adapters.users_for_message.UserMesViewHolder
+
+class AdapterUserMes : ListAdapter<User, UserMesViewHolder>(UserMesItemDiffCallback()) {
+
+    var onUserClickListener: ((User)->Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserMesViewHolder {
+        val binding = ItemUserMesBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserMesViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserMesViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.name.text = item.name
+        holder.flatNum.text = item.flat.toString()
+        holder.binding.root.setOnClickListener {
+            onUserClickListener?.invoke(item)
+        }
+        holder.binding.ivUserIcon.setImageResource(R.drawable.bottom_nav_chats)
+    }
+}
--- app/src/main/java/com/example/homehelper/presentation/screens/chats/NewMessageFragment.kt
+++ app/src/main/java/com/example/homehelper/presentation/screens/chats/NewMessageFragment.kt
@@ -5,9 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.homehelper.R
+import com.example.homehelper.databinding.FragmentChatBinding
+import com.example.homehelper.presentation.HomeHelperApp
+import com.example.homehelper.presentation.adapters.messages.AdapterMessages
+import com.example.homehelper.presentation.viewmodels.ChatViewModel
+import com.example.homehelper.presentation.viewmodels.ViewModelFactory
+import javax.inject.Inject
 
 class NewMessageFragment : Fragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    @Inject
+    lateinit var adapterMessages: AdapterMessages
+
+    private val chatViewModel: ChatViewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[ChatViewModel::class.java]
+    }
+
+    private val component by lazy {
+        (requireActivity().application as HomeHelperApp).component
+    }
+
+    private var _binding: FragmentChatBinding? = null
+    private val binding: FragmentChatBinding
+        get() = _binding ?: throw RuntimeException("FragmentChatBinding = null!")
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,

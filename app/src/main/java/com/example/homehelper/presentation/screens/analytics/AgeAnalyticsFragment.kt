711f65fbe1ea4b92a1497d9882702e65e1f39091
RedMuri <kartoev.04@bk.ru>
2022-11-22 15:13:33 +0300

    add: added binding in AgeAnalyticsFragment.kt

--- app/src/main/java/com/example/homehelper/presentation/screens/analytics/AgeAnalyticsFragment.kt
+++ app/src/main/java/com/example/homehelper/presentation/screens/analytics/AgeAnalyticsFragment.kt
@@ -6,8 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.homehelper.R
+import com.example.homehelper.databinding.FragmentAddEventBinding
+import com.example.homehelper.databinding.FragmentAgeAnalyticsBinding
+import org.eazegraph.lib.models.PieModel
 
 class AgeAnalyticsFragment : Fragment() {
+
+    private var _binding: FragmentAgeAnalyticsBinding? = null
+    private val binding: FragmentAgeAnalyticsBinding
+        get() = _binding ?: throw RuntimeException("FragmentAgeAnalyticsBinding = null!")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,13 +24,21 @@ class AgeAnalyticsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_age_analytics, container, false)
+    ): View {
+        _binding = FragmentAgeAnalyticsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.pieChart.addPieSlice(PieModel(12F,R.color.analytics_age_first))
+        binding.pieChart.addPieSlice(PieModel(45F,R.color.analytics_age_second))
+        binding.pieChart.addPieSlice(PieModel(18F,R.color.analytics_age_third))
     }
 
     companion object {
 
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             AgeAnalyticsFragment()
     }
 }

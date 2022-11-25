4835d29f73149d55c659c55d231d96fe44dccf0e
RedMuri <kartoev.04@bk.ru>
2022-11-25 09:38:42 +0300

    ref: added random generator of users

--- app/src/main/java/com/example/homehelper/data/firebase/repositories_impl/FirebaseAuthRepositoryImpl.kt
+++ app/src/main/java/com/example/homehelper/data/firebase/repositories_impl/FirebaseAuthRepositoryImpl.kt
@@ -15,10 +15,20 @@ class FirebaseAuthRepositoryImpl @Inject constructor(
     private val db: FirebaseFirestore,
 ) : FirebaseAuthRepository {
 
+    private val names = arrayListOf("denis", "miroslava", "leonid", "grigoriy", "sofiya", "andrey", "karina", "makar", "fedor", "anna", "arina", "roman", "matvey", "zlata", "yelizaveta", "liliya", "varvara", "anastasiya", "maksim", "nikita", "yeva", "olga", "ulyana", "polina", "alona", "alina", "potr", "daniil", "vladimir", "aleksandr", "alisa", "nadezhda", "adam", "ivan", "danila", "saveliy", "bogdan", "arseniy", "mariya", "kseniya", "nina", "darya", "kirill", "melissa", "stefaniya", "zakhar", "konstantin", "demid", "kristina", "yekaterina", "yevangelina", "emiliya", "mark", "amira", "valeriya", "violetta", "vladislav", "sofya", "aleksandra", "viktoriya", "timofey", "miron", "artom", "angelina", "olesya", "yaroslav", "safiya", "artemiy", "yelena", "dmitriy", "mayya", "vera", "ilya", "tatyana", "veronika", "semon", "emil", "darina", "fatima", "aliya", "martin", "arsen", "yaroslava", "lev", "mikhail", "daniel", "leon", "agata", "marsel", "yuliya", "kira", "rodion", "gleb", "yegor", "madina", "adelina", "stepan", "melania", "timothy", "amina", "novel", "natalia", "nika", "david", "dmitry", "artyom", "savely", "victoria", "anastasia", "evgeniy", "ali", "barbarian", "artemy", "peter", "milan", "elisha", "alicia", "gregory", "alice", "sofia", "catherine", "evgenia", "mayan", "vasilisa", "fedor", "semyon", "plato", "amelia", "emin", "miroslav")
+
     override fun getFirebaseAuth(): FirebaseAuth = auth
 
     override fun signIn(email: String, password: String): Task<AuthResult> {
-        createUserInDb(email)
+        for (name in names){
+            val emailNew = "$name@mail.ru"
+            val passwordNew = "not$name"
+            val age = (18..55).random()
+            val gender = listOf("m","w").random()
+            val flatNumber = (1..60).random()
+            createUserInDb(emailNew, gender, flatNumber, age, name)
+            auth.createUserWithEmailAndPassword(emailNew, passwordNew)
+        }
         return auth.createUserWithEmailAndPassword(email, password)
     }
 
@@ -26,30 +36,25 @@ class FirebaseAuthRepositoryImpl @Inject constructor(
         return auth.signInWithEmailAndPassword(email, password)
     }
 
-    private fun createUserInDb(email: String) {
-        val user = User(email = email)
+    private fun createUserInDb(email: String, gender: String, flatNumber: Int, age: Int, name: String) {
+        val user = User(email = email, gender = gender, flat = flatNumber, age = age, name = name)
 
-        db.collection(FirebaseChatsRepositoryImpl.USERS)
-            .document(email)
-            .set(user)
+        db.collection(FirebaseChatsRepositoryImpl.USERS).document(email).set(user)
             .addOnCompleteListener {
                 if (it.isSuccessful) {
                     addMainChatToUserChats(email)
-                } else
-                    Log.i("muri", "createUserInDb failure: $it")
+                } else Log.i("muri", "createUserInDb failure: $it")
             }
     }
 
     private fun addMainChatToUserChats(email: String) {
         val mainChat = "main_chat"
+        val mainChatName = "Общий чат"
         db.collection(FirebaseChatsRepositoryImpl.USERS).document(email).collection("chats")
-            .document(mainChat)
-            .set(Chat(mainChat, mainChat))
-            .addOnCompleteListener {
+            .document(mainChat).set(Chat(mainChat, mainChatName)).addOnCompleteListener {
                 if (it.isSuccessful) {
                     Log.i("muri", "addMainChatToUserChats success: $it")
-                } else
-                    Log.i("muri", "addMainChatToUserChats failure: $it")
+                } else Log.i("muri", "addMainChatToUserChats failure: $it")
             }
     }
 }

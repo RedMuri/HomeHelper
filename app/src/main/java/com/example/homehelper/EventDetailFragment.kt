9313e748e3d4cfdd29c345c99595f9e18a4a6853
RedMuri <kartoev.04@bk.ru>
2022-11-26 10:30:04 +0300

    add: added fragment_event_detail.xml

---
+++ app/src/main/java/com/example/homehelper/EventDetailFragment.kt
@@ -0,0 +1,31 @@
+package com.example.homehelper
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EventDetailFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event_detail, container, false)
+    }
+
+    companion object {
+
+
+
+        fun newInstance(event: Event) =
+            EventDetailFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                }
+            }
+    }
+}
